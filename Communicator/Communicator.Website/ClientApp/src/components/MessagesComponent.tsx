871a17a3f60f6a2acd2b9ea8beacbd2374ceb7d2
Michał <dela_m@interia.pl>
2020-03-22 21:31:42 +0100

    Added messages Template

--- Communicator/Communicator.Website/ClientApp/src/components/MessagesComponent.tsx
+++ Communicator/Communicator.Website/ClientApp/src/components/MessagesComponent.tsx
@@ -5,11 +5,12 @@ import { ApplicationState } from '../store/index';
 import { connect } from 'react-redux';
 import * as MessagesStore from '../store/Messages';
 import { MessagesState } from '../store/Messages';
-import '../styles/Login.css';
 import { CookiesManager } from '../Managers/CookiesManager';
 import { Status } from '../store/Models/Status';
 import { ApplicationUser } from '../store/Models/ApplicationUser';
 import { Link } from 'react-router-dom';
+import '../styles/Login.css';
+import '../styles/ChatMessages.css';
 
 
 type MessagesProps =
@@ -57,24 +58,71 @@ class MessagesComponent extends React.Component<MessagesProps, MessagesState> {
                     </Navbar>
                 </header>
 
+                <div className="container">
+                    <div className="messaging">
+                        <div className="inbox_msg">
+                            {/*Left Panel */}
+                            <div className="inbox_people">
+                                <div className="headind_srch">
+                                    <div className="recent_heading">
+                                        <h4>Recent</h4>
+                                    </div>
+                                    <div className="srch_bar">
+                                        <div className="stylish-input-group">
+                                            <input type="text" className="search-bar" placeholder="Search" />
+                                            <span className="input-group-addon">
+                                                <button type="button"> <i className="fa fa-search" aria-hidden="true"></i> </button>
+                                            </span> </div>
+                                    </div>
+                                </div>
+                                <div className="inbox_chat">
+                                    <div className="chat_list active_chat">
+                                        <div className="chat_people">
+                                            <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                            <div className="chat_ib">
+                                                <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
+                                                <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="chat_list">
+                                        <div className="chat_people">
+                                            <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                            <div className="chat_ib">
+                                                <h5>Sunil Rajput <span className="chat_date">Dec 25</span></h5>
+                                                <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                <div id="MessagesComponent">
-                    <div id="LeftPanel">
-                        LeftPanel
-                    </div>
-                    <div id="RightPanel">
-                        <div id="TopRightInfo">
-                            TopRightInfo
+                            {/* Right Panel */}
+                            <div className="mesgs">
+                                <div className="msg_history">
+                                    <div className="incoming_msg">
+                                        <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                        <div className="received_msg">
+                                            <div className="received_withd_msg">
+                                                <p>Test which is a new approach to have all solutions</p>
+                                                <span className="time_date"> 11:01 AM    |    June 9</span></div>
+                                        </div>
+                                    </div>
+                                    <div className="outgoing_msg">
+                                        <div className="sent_msg">
+                                            <p>Test which is a new approach to have allsolutions</p>
+                                            <span className="time_date"> 11:01 AM    |    June 9</span> </div>
+                                    </div>
+                                </div>
+                                <div className="type_msg">
+                                    <div className="input_msg_write">
+                                        <input type="text" className="write_msg" placeholder="Type a message" />
+                                        <button className="msg_send_btn" type="button"><i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
-                        <div id="MessagesPanel">
-                            MessagesPanel
-                        </div>
-                        <div id="SendMessagePanel">
-                            SendMessagePanel
-                        </div>
-                    </div>
-                </div>
+                    </div></div>
             </React.Fragment>
         );
     }
